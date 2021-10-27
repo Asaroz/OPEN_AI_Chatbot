80b4f3442a551ae4b7d60e6ee17f373bd97b1f61
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-10-27 15:17:32 +0200

    First Push

--- Backend/index.js
+++ Backend/index.js
@@ -2,17 +2,38 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import OpenAI from 'openai-api';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 dotenv.config();
-
-app.get('/', (req, res) => {
-    console.log("this shit works")
-});
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const openAI = new OpenAI(OPENAI_API_KEY)
 
 
-app.listen(3001, () =>
-    console.log('Example app listening on port 3000!'),
+app.get('/test', (req, res) => {});
+
+app.post('/', async(req, res) => {
+    const gptResponse = await openAI.complete({
+        engine: 'davinci',
+        prompt: req.body.question,
+        maxTokens: 5,
+        temperature: 0.9,
+        topP: 1,
+        presencePenalty: 0,
+        frequencyPenalty: 0,
+        bestOf: 1,
+        n: 1,
+        stream: false,
+        stop: ['\n', "testing"]
+    });
+
+    res.send(gptResponse.data.choices[0].text);
+
+})
+
+
+app.listen(4000, () =>
+    console.log('Example app listening on port 4000!'),
 );
