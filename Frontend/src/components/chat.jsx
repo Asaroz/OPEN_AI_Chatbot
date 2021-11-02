72e689b2289b2f6e8c00ade3d417a78b2f9b4aba
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-11-02 13:16:33 +0100

    Dropdown Menu

--- Frontend/src/components/chat.jsx
+++ Frontend/src/components/chat.jsx
@@ -1,14 +1,16 @@
 import { React, useState } from "react";
-import axios from 'axios';
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import './chat.css'
+import axios from "axios";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+import "./chat.css";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: "http://localhost:4000",
   timeout: 10000,
-  headers: { 'X-Custom-Header': 'foobar' }
+  headers: { "X-Custom-Header": "foobar" },
 });
 
 function Chat() {
@@ -16,57 +18,72 @@ function Chat() {
     setQuestion(e.target.value);
   };
 
-  const initialArray = []
-  const [chat,setChat] = useState (initialArray)
+  const initialArray = [];
+  const [chat, setChat] = useState(initialArray);
   const [question, setQuestion] = useState("");
   // const [answer, setAnswer] = useState("");
 
   const send = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     instance
       .post("/", {
-        question: question
+        question: question,
       })
       .then(function (response) {
-        console.log(response.data)
+        console.log(response.data);
         // setAnswer(response.data);
-        setChat( ()=> {
-          chat.push({You: "You: "+question, Bot:response.data})
+        setChat(() => {
+          chat.push({ You: "You: " + question, Bot: response.data });
           console.log(chat);
-          return chat
-        })
-       setQuestion("")
+          return chat;
+        });
+        setQuestion("");
       })
       .catch(function (error) {
         console.log(error);
       });
   };
 
+  const [mood, setMood] = useState("");
+  const handleMood = (e) => {
+    console.log(e);
+    setMood(e);
+    
+  };
 
   return (
     <div>
+      <DropdownButton variant="info" title="Select the Robot's mood" onSelect={handleMood}>
+        <Dropdown.Item className="dropdown" eventKey="happy">Happy and likes humans</Dropdown.Item>
+        <Dropdown.Item eventKey="rude">Rude and dislikes humans</Dropdown.Item>
+        <Dropdown.Item eventKey="robotic">A robotic robot</Dropdown.Item>
+      </DropdownButton>
+
       <textarea
         className="form-control"
-        className= "chatbox"
+        className="chatbox"
         readOnly
         rows="20"
-        value= {chat.map(x=>{
+        value={chat.map((x) => {
           const q = x.You;
           const a = x.Bot;
-          return q + "\n" + a +"\n"
+          return q + "\n" + a + "\n";
         })}
-      >
-        
-      </textarea>
+      ></textarea>
       <Form className="mb-3" onSubmit={send}>
         <FormControl
           onChange={handleChange}
           placeholder="Chat here"
           aria-label="Recipient's username"
           aria-describedby="basic-addon2"
-          value = {question}
+          value={question}
         />
-        <Button className= "button" type="submit" variant="outline-secondary" id="button-addon2">
+        <Button
+          className="button"
+          type="submit"
+          variant="outline-secondary"
+          id="button-addon2"
+        >
           Send
         </Button>
       </Form>
--- Frontend/src/components/login.jsx
+++ Frontend/src/components/login.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
-import './login.css';
-
+import "./login.css";
 
 export default function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const setUser = props.setUser
+  const setUser = props.setUser;
 
   const axiosE = axios.create({
     baseURL: "http://localhost:4000",
@@ -22,8 +21,8 @@ export default function Login(props) {
         password: password,
       })
       .then((response) => {
-        if(response.data.succes){
-          setUser(response.data.user)
+        if (response.data.succes) {
+          setUser(response.data.user);
         }
       })
       .catch(function (error) {
@@ -31,50 +30,55 @@ export default function Login(props) {
       });
   }
 
-  function handleRegister(){
-  axiosE.post("/register", {
-    email: email,
-    password: password,
-  })
-  .then((response) => {
-    console.log(response);
-    if(response.data.success){
-      console.log('whats good');
-     alert("Registration succesful, please login now")
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
+  function handleRegister() {
+    axiosE
+      .post("/register", {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        if (response.data.success) {
+          alert("Registration succesful, please login now");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-  return (<div> <form className="form" onSubmit={handleSubmit}>
-      <label>
-        <p>Username</p>
-        <input
-        className="input"
-          type="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-      </label>
-      <label>
-        <p>Password</p>
-        <input
-        className="input"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <div>
-        <button className="button" type="submit">Login</button>
-        
-      </div>
-    </form>
-    <button className="button"  onClick={handleRegister}>Register</button></div>
-   
+  return (
+    <div>
+      {" "}
+      <form className="form" onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input
+            className="input"
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          <p>Password</p>
+          <input
+            className="input"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <div>
+          <button className="button" type="submit">
+            Login
+          </button>
+        </div>
+      </form>
+      <button className="button" onClick={handleRegister}>
+        Register
+      </button>
+    </div>
   );
 }
